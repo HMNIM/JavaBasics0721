73d5559bc53b6ad7441bbbf5950df8e8cda08141
makkenaharitha <makkena.haritha7@gmail.com>
2019-07-22 16:30:24 -0700

    Arrays

---
+++ src/com/javabasics/arrays/BinarySearchOnArray.java
@@ -0,0 +1,12 @@
+package com.javabasics.arrays;
+
+import java.util.Arrays;
+
+public class BinarySearchOnArray {
+    public static void main(String args[])
+    {
+        char[] alphabetsArr = {'s', 'g', 'j' , 'k' , 'n' , 'm' , 'o', 'p'};
+        int index = Arrays.binarySearch(alphabetsArr, 0 ,alphabetsArr.length-1, 'n');
+        System.out.println(index);
+    }
+}
